4ff2fda14087147fd8bc7e0f86f80bf01b70a2f1
Mahmoud Farghali <101347618@georgebrown.ca>
2023-02-02 17:35:59 -0500

    resturant schema created

---
+++ Resturant.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const ResturantSchema = new mongoose.Schema({
+    name: {
+        type : String,
+        required : [true, 'provide resturant name'],
+        lowercase : true
+    },
+    cuisines : [String],
+    city: {
+        type : String, 
+        required : [true, 'provide city name']
+        loweercase : 
+    }
+
+})
